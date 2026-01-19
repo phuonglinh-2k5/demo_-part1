260bd4f24663993bb973b38bc67c1dac91be3f7c
phuonglinh-2k5 <linhphuongpham2k5@gmail.com>
2026-01-19 21:32:40 +0700

    commit part1

---
+++ Assets/Space Shooter Template FREE/Scripts/PlayerMovement.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed = 5f;
+
+    void Update()
+    {
+        // 1. Lấy vị trí chuột (world)
+        Vector3 mousePos = Input.mousePosition;
+        mousePos.z = 10f; // khoảng cách từ camera
+        Vector3 target =
+            Camera.main.ScreenToWorldPoint(mousePos);
+
+        target.z = 0f;
+
+        // 2. Player đuổi theo chuột
+        transform.position = Vector3.MoveTowards(
+            transform.position,
+            target,
+            speed * Time.deltaTime
+        );
+    }
+}
