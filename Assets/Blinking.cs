fd3445f76d8a4248a7e28700f2782169ca3f8429
phuonglinh-2k5 <linhphuongpham2k5@gmail.com>
2026-01-23 17:35:08 +0700

    Upload demo part 2

--- Assets/Blinking.cs
+++ Assets/Blinking.cs
@@ -1,8 +1,14 @@
 ﻿using UnityEngine;
 
+[RequireComponent(typeof(SpriteRenderer))]
 public class Blinking : MonoBehaviour
 {
     private SpriteRenderer spriteRenderer;
+
+    [Tooltip("Time between blink on/off")]
+    public float blinkInterval = 0.1f;
+
+    private float nextBlinkTime;
 
     void Awake()
     {
@@ -13,12 +19,18 @@ public class Blinking : MonoBehaviour
             Debug.LogError(
                 "Blinking script must be attached to a GameObject with SpriteRenderer!"
             );
-            enabled = false; // TẮT SCRIPT để tránh spam lỗi
+            enabled = false;
         }
     }
 
     void Update()
     {
-        spriteRenderer.enabled = !spriteRenderer.enabled;
+        if (!spriteRenderer) return;
+
+        if (Time.time >= nextBlinkTime)
+        {
+            spriteRenderer.enabled = !spriteRenderer.enabled;
+            nextBlinkTime = Time.time + blinkInterval;
+        }
     }
 }
